0e19de60071f5e655ee0c03f054f2aecef706696
Ajith Kumar J <ajithgoku333@gmail.com>
2022-04-15 20:21:21 +0530

    1. Added devise gem. 2. Created user model using devise gem. 3. Signup, signIn & Edit profile pages and functionality completed. Have used bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # devise_for :users => this is used for without users controller folder
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  # Use root "home#index" (or) root to: "home#index" no diff
+  root to: "home#index"
+  get 'home/index'
+  resources :users
 end
