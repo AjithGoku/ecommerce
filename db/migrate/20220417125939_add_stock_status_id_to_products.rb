d4f94974752f7e7fde444029e323eafde1fee702
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2022-04-17 19:08:21 +0530

    Update 20220417125939_add_stock_status_id_to_products.rb

--- db/migrate/20220417125939_add_stock_status_id_to_products.rb
+++ db/migrate/20220417125939_add_stock_status_id_to_products.rb
@@ -1,4 +1,4 @@
-class AddStockStatusIdToProducts < ActiveRecord::Migration[5.2]
+class AddStockStatusIdToProducts < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :stock_status_id, :integer
     add_index :products, :stock_status_id
