408d1031570e2acdc02571a396ac14c8d59b1b7f
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2022-04-17 19:07:22 +0530

    Update 20220414112036_create_product_categories.rb

--- db/migrate/20220414112036_create_product_categories.rb
+++ db/migrate/20220414112036_create_product_categories.rb
@@ -1,4 +1,4 @@
-class CreateProductCategories < ActiveRecord::Migration[5.2]
+class CreateProductCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :product_categories do |t|
       t.string :name
