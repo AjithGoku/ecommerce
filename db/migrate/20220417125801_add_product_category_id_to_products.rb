c3d821e2f7bfe44269f4bd50d42bee403470589a
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2022-04-17 19:08:03 +0530

    Update 20220417125801_add_product_category_id_to_products.rb

--- db/migrate/20220417125801_add_product_category_id_to_products.rb
+++ db/migrate/20220417125801_add_product_category_id_to_products.rb
@@ -1,4 +1,4 @@
-class AddProductCategoryIdToProducts < ActiveRecord::Migration[5.2]
+class AddProductCategoryIdToProducts < ActiveRecord::Migrationv[7.0]
   def change
     add_column :products, :product_category_id, :integer
     add_index :products, :product_category_id
