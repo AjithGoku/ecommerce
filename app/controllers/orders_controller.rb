b2f3df87774a2255d0a652eaa9a1c18bbce2cf17
Devi S <anjali.devi@accenture.com>
2022-04-18 13:28:26 +0530

    code clean up done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,74 +1,51 @@
 class OrdersController < ApplicationController
 
-def index
-
+#Display all the orders
+  def index
     @orders = Order.all
-
   end
 
-
-
+#show the orderitems for specific order
   def show
-
     @orders = Order.find(params[:id])
     @orderitem = OrderItem.where("order_id = ?", params[:id])
-
   end
-
-
 
   def new
 
   end
 
-
-
   def edit
-
     @orders = Order.find(params[:id])
-
-
-
   end
 
   def update
-
     order = Order.find(params[:id])
-
     order.update(order_params)
-
-   
-
     redirect_to root_path
+  end
 
- end
+  def create
+    order = Order.new(employee_params)
+    order.save
+    redirect_to root_path
+  end
 
-#   def create
+#To destroy the specified order
+  def destroy
+    @orderitem = OrderItem.where("order_id = ?", params[:id])
+    @orderitem.destroy_all
+    @order = Order.find(params[:id])
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_index_path, notice: "Order and associated order items are removed." }  
+    end
+  end
 
-#     order = Order.new(employee_params)
+  private
 
-#     order.save
-
-#     redirect_to root_path
-
-# end
-
-def destroy
-
-  order = Order.find(params[:id])
-
-  order.destroy
-
-  redirect_to root_path
-
-end
-
-# private
-
-# def order_params
-
-#     params.require(:order).permit(:name, :position)
-
-# end
+  def order_params
+      params.require(:order).permit(:name, :position)
+  end
 
 end
