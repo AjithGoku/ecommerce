0c28b2f9f325816f8eaf0a06b9e50d7683b83a10
Devi S <anjali.devi@accenture.com>
2022-04-28 15:05:59 +0530

    functionality changes in product delivery

--- app/controllers/ecommerce_controller.rb
+++ app/controllers/ecommerce_controller.rb
@@ -1,5 +1,14 @@
 class EcommerceController < ApplicationController
     def index
         @orders = Order.where("user_id= ?", current_user.id)
+        @product_categories=ProductCategory.all
+        @stock_status=StockStatus.all
+        @order_items=OrderItem.all.limit(5)
+        # p @order_item
+        # p @order_item.product
+        # p @order_item.order
+        # p @order_item.order.user
     end
+
+    
 end
